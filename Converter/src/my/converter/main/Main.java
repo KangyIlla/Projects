a39e543058e2028cd7985c635f8576886cee1c18
kangeyai <31408743+kangeyai@users.noreply.github.com>
2018-08-21 20:17:38 +0200

    Have added all the different type of converting methods

--- Converter/src/my/converter/main/Main.java
+++ Converter/src/my/converter/main/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	static String[] hexSymbols = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+	static String[] hexSymbols = {"0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 
 	public static void main(String[] args) {
 		int number;
@@ -18,15 +18,22 @@ public class Main {
 			System.out.println("Error: Not a positive integer");
 		} else {
 
-			System.out.print("Convert to binary is:");
-			//System.out.print(binaryform(number));
+			System.out.println("Convert to binary is:");
 			System.out.println(convertToBinary(number));
+			System.out.println("Converted back to decimal: "+binaryToDecimal(convertToBinary(number)));
+			System.out.println("Convert to hex is:");
+			System.out.println(convertToHex(number));
+			System.out.println("Converted back to decimal: "+hexToDecimal(convertToHex(number)));
+			System.out.println("Convet to octan is:");
+			System.out.println(convertToOctan(number));
+			System.out.println("Converted back to decimal: "+octanToDecimal(convertToOctan(number)));
+			
 		}
 	}
 
 	private static String convertToBinary(int number) {
 		
-		String binarynumber  = " ";
+		String binarynumber  = "";
 		
 		while(number > 0) {
 			int reminder = number % 2;
@@ -38,14 +45,43 @@ public class Main {
 	}
 	
 	private static String convertToHex(int number) {
-		String hexnumber  = " ";
+		String hexnumber  = "";
 
 		while(number > 0) {
 			int reminder = number % 16;
-			hexnumber = String.valueOf(reminder) + hexnumber; 
+			hexnumber = hexSymbols[reminder] + hexnumber; 
 			number = (number >> 4);	
 		}
 
 		return hexnumber; 
 	}
+	
+	private static String convertToOctan(int number) {
+		String octannumber  = "";
+
+		while(number > 0) {
+			int reminder = number % 8;
+			octannumber = hexSymbols[reminder] + octannumber; 
+			number = (number >> 3);	
+		}
+
+		return octannumber; 
+	}
+	
+	private static String octanToDecimal(String number) {
+		int octValue = Integer.parseInt(number, 8);
+		return String.valueOf(octValue);
+	}
+	
+	private static String hexToDecimal(String number) {
+		int hexValue = Integer.parseInt(number, 16);
+		return String.valueOf(hexValue);
+	}
+	
+	private static String binaryToDecimal(String number) {
+		int binaryValue = Integer.parseInt(number, 2);
+		return String.valueOf(binaryValue);
+	}
+	
+	
 }
