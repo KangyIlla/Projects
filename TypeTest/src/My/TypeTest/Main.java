1625d7822f41ed35f24e4fbebce87dfb5eea3015
kangeyai <31408743+kangeyai@users.noreply.github.com>
2018-08-23 20:13:10 +0200

    Added words per minutt on the highscore + some minor changes

--- TypeTest/src/My/TypeTest/Main.java
+++ TypeTest/src/My/TypeTest/Main.java
@@ -19,10 +19,12 @@ public class Main {
 	private static JTextPane text;
 	private static JTextField writing;
 	private static String [] book = new String[25];
-	private static int counter = 0;
 	private static int correctWords = 0;
 	private static int countChar = 0;
 	private static int countWords = 0;
+	private static boolean typeStarted = false;
+	private static double startTime = 0.0;
+	private static double endTime = 0.0;
 
 	public static void main(String [] args) {
 		JFrame window = new JFrame("TypeTest");
@@ -91,11 +93,11 @@ public class Main {
 		text.setText(textOut);
 	}
 
-	public static void getHighScore() {
+	public static void getHighScore(double timediff, int words) {
 		JFrame highScore = new JFrame("HighScore");
-		highScore.setPreferredSize(new Dimension(200,150));
-		highScore.setMaximumSize(new Dimension(200,150));
-		highScore.setMinimumSize(new Dimension(200,150));
+		highScore.setPreferredSize(new Dimension(350,200));
+		highScore.setMaximumSize(new Dimension(350,200));
+		highScore.setMinimumSize(new Dimension(350,200));
 
 		highScore.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		highScore.setResizable(false);
@@ -106,44 +108,68 @@ public class Main {
 		GridBagConstraints c2 = new GridBagConstraints();
 		c2.insets = new Insets(10,10,10,10);
 
-		label = new JLabel("Higscore",10);
+		label = new JLabel("Congratulations, you have finished the TypeTest!");
 		c2.fill = GridBagConstraints.HORIZONTAL;
 		c2.gridx = 0;
 		c2.gridy = 0;
-		panel2.add(label, c2);	
+		panel2.add(label, c2);
+		
+		JTextField time = new JTextField();
+		double wordsPerMinute = (timediff/words)*60000;
+		String out = "Words Per Minute was: " + wordsPerMinute;
+		time.setText(out);
+		time.setEditable(false);
+		c2.fill = GridBagConstraints.HORIZONTAL;
+		c2.gridx = 0;
+		c2.gridy = 1;
+		panel2.add(time, c2);
 
 		highScore.setVisible(true);
 	}
+	
+	public static void setColour(boolean colour, int charCount, int wordCount) {
+		SimpleAttributeSet attrs = new SimpleAttributeSet();
+		SimpleAttributeSet attrs2 = new SimpleAttributeSet();
+		StyleConstants.setForeground(attrs, Color.decode("#228B22"));
+		StyleConstants.setForeground(attrs2, Color.decode("#8B0000"));
+
+		StyledDocument sdoc = text.getStyledDocument();
+		
+		if(colour) {
+			sdoc.setCharacterAttributes(charCount, book[wordCount].length() + 1, attrs, false);
+		} else {
+			sdoc.setCharacterAttributes(charCount, book[wordCount].length() + 1, attrs2, false);
+		}
+	}
+	
 
 	static class KeyPress implements KeyListener{
 
 		public void keyPressed(KeyEvent k) {
-
-
-			SimpleAttributeSet attrs = new SimpleAttributeSet();
-			SimpleAttributeSet attrs2 = new SimpleAttributeSet();
-			StyleConstants.setForeground(attrs, Color.decode("#228B22"));
-			StyleConstants.setForeground(attrs2, Color.decode("#8B0000"));
-
-			StyledDocument sdoc = text.getStyledDocument();
-
+			if(!typeStarted) {
+				typeStarted = true;
+				startTime = System.currentTimeMillis();
+			}
+			
+			
 			if(countWords < book.length) {
 				String temp = writing.getText().trim();
 				if(k.getKeyCode() == 32) {
 					if(!temp.equals(" ")) {
 						if(temp.equals(book[countWords])) {	
-							sdoc.setCharacterAttributes(countChar, book[countWords].length() + 1, attrs, false);
+							setColour(true,countChar,countWords);
 							countChar += temp.length() + 1;
 							correctWords++;
 							countWords++;
 							writing.setText("");
 							if(countWords == book.length) {
+								endTime = System.currentTimeMillis(); 
 								writing.setEditable(false);
 								System.out.println("You are finished with the game");
-								getHighScore();
+								getHighScore(endTime-startTime,correctWords);
 							}
 						} else {
-							sdoc.setCharacterAttributes(countChar, book[countWords].length() + 1, attrs2, false);
+							setColour(false,countChar,countWords);
 							System.out.println("Wrong");
 						}
 					}	
